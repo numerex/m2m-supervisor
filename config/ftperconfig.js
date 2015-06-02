6658e191c0b9b0e24605e55a0cc4ed9a0a303611
Andrew Wolverton <awolverton@gmail.com>
2015-06-02 15:52:04 -0500

    initial commit of ftper branch

---
+++ config/ftperconfig.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.ftp = {
+        username: "bbfota",
+        password: "bbftp",
+        host: "gwaas-prod-sv3.numerexfast.com",
+        path: "/etc/m2m-supervisor"
+}
+
+exports.redis = {
+        devicenamekeyhash: "m2m-config",
+        devicenamekey: "gateway:imei"
+}
+
