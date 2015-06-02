59b4a21fbf040b2ea01ce41f8bb7e6f69807718b
Andrew Wolverton <awolverton@gmail.com>
2015-06-02 15:57:10 -0500

    tidy up

--- config/ftperconfig.js
+++ config/ftperconfig.js
@@ -1,14 +1,13 @@
 'use strict';
 
 exports.ftp = {
-        username: "bbfota",
-        password: "bbftp",
-        host: "gwaas-prod-sv3.numerexfast.com",
-        path: "/etc/m2m-supervisor"
+    username: "bbfota",
+    password: "bbftp",
+    host: "gwaas-prod-sv3.numerexfast.com",
+    path: "/etc/m2m-supervisor"
 }
 
 exports.redis = {
-        devicenamekeyhash: "m2m-config",
-        devicenamekey: "gateway:imei"
+    devicenamekeyhash: "m2m-config",
+    devicenamekey: "gateway:imei"
 }
-
